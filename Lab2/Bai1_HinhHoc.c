0caaae4cd63bbf4e3be70623af2a7562cdb7d264
nguyenviettruong8186-crypto <nguyenviettruong8186@gmail.com>
2026-07-19 10:44:20 +0700

    Hoan thanh Lab2 Bai 1

--- Lab2/Bai1_HinhHoc.c
+++ Lab2/Bai1_HinhHoc.c
@@ -1,35 +1,33 @@
 #include <stdio.h>
 
-// Định nghĩa hằng số PI theo yêu cầu đề bài
+// Bắt buộc phải có dòng này theo đề bài nha
 #define PI 3.14159
 
 int main() {
-    // --- 1. XỬ LÝ HÌNH CHỮ NHẬT ---
-    double chieuDai, chieuRong;
-    printf("--- Tinh toan Hinh Chu Nhat ---\n");
-    printf("Nhap vao chieu dai: ");
-    scanf("%lf", &chieuDai);
-    printf("Nhap vao chieu rong: ");
-    scanf("%lf", &chieuRong);
+    float dai, rong, r;
+    float cvHCN, dtHCN, cvTron, dtTron;
 
-    double chuViHCN = (chieuDai + chieuRong) * 2;
-    double dienTichHCN = chieuDai * chieuRong;
+    // 1. Tinh hinh chu nhat
+    printf("Nhap chieu dai: ");
+    scanf("%f", &dai);
+    printf("Nhap chieu rong: ");
+    scanf("%f", &rong);
 
-    printf("Chu vi Hinh Chu Nhat: %.2lf\n", chuViHCN);
-    printf("Dien tich Hinh Chu Nhat: %.2lf\n\n", dienTichHCN);
+    cvHCN = (dai + rong) * 2;
+    dtHCN = dai * rong;
 
+    printf("Chu vi HCN: %.2f\n", cvHCN);
+    printf("Dien tich HCN: %.2f\n", dtHCN);
 
-    // --- 2. XỬ LÝ HÌNH TRÒN ---
-    double banKinh;
-    printf("--- Tinh toan Hinh Tron ---\n");
-    printf("Nhap vao ban kinh: ");
-    scanf("%lf", &banKinh);
+    // 2. Tinh hinh tron
+    printf("Nhap ban kinh: ");
+    scanf("%f", &r);
 
-    double chuViTron = 2 * PI * banKinh;
-    double dienTichTron = PI * banKinh * banKinh;
+    cvTron = 2 * PI * r;
+    dtTron = PI * r * r;
 
-    printf("Chu vi Hinh Tron: %.5lf\n", chuViTron);
-    printf("Dien tich Hinh Tron: %.5lf\n", dienTichTron);
+    printf("Chu vi hinh tron: %.2f\n", cvTron);
+    printf("Dien tich hinh tron: %.2f\n", dtTron);
 
     return 0;
 }
