ee1d931aa0a5c0f563a848661a246286efaa2432
nguyenviettruong8186-crypto <nguyenviettruong8186@gmail.com>
2026-07-19 11:03:00 +0700

    Hoan thanh Lab2 Bai 3

--- Lab2/Bai3_Phuongtrinh.c
+++ Lab2/Bai3_Phuongtrinh.c
@@ -1,28 +1,13 @@
 #include <stdio.h>
-
 int main() {
-    // Khai báo các hệ số và nghiệm dưới dạng số thực (float) để kết quả chính xác
-    float a, b;
+    float a,b;
     float x;
-
-    printf("--- GIAI PHUONG TRINH BAC NHAT ax + b = 0 ---\n");
-    
-    // Nhập hệ số a từ bàn phím
-    printf("Nhap he so a (a khac 0): ");
-    scanf("%f", &a);
-
-    // Nhập hệ số b từ bàn phím
-    printf("Nhap he so b: ");
-    scanf("%f", &b);
-
-    // Do đề bài giả định hệ số a nhập vào luôn khác 0
-    // Công thức tính nghiệm: ax + b = 0 => ax = -b => x = -b / a
-    x = -b / a;
-
-    // Hiển thị kết quả ra màn hình (làm tròn 2 chữ số thập phân cho đẹp)
-    printf("\n--------------------------------------------\n");
-    printf("Phuong trinh %.2fx + %.2f = 0\n", a, b);
-    printf("Co nghiem x = %.2f\n", x);
-
+    printf("Nhaphesoa:");
+    scanf("%f",&a);
+    printf("Nhaphesob:");
+    scanf("%f",&b);
+    x=-b/a;
+    printf("Nghiemxla: %f\n",x);
     return 0;
+
 }
