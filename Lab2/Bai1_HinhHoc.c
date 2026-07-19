7aa9b057421ad4fb72c39e33961dfb4c3a17f4ea
nguyenviettruong8186-crypto <nguyenviettruong8186@gmail.com>
2026-07-19 14:07:03 +0700

    Cap nhat lai Bai 1 va hoan thanh Lab 2

--- Lab2/Bai1_HinhHoc.c
+++ Lab2/Bai1_HinhHoc.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
-
-// Bắt buộc phải có dòng này theo đề bài nha
 #define PI 3.14159
-
 int main() {
-    float dai, rong, r;
-    float cvHCN, dtHCN, cvTron, dtTron;
-
-    // 1. Tinh hinh chu nhat
-    printf("Nhap chieu dai: ");
-    scanf("%f", &dai);
-    printf("Nhap chieu rong: ");
-    scanf("%f", &rong);
-
-    cvHCN = (dai + rong) * 2;
-    dtHCN = dai * rong;
-
-    printf("Chu vi HCN: %.2f\n", cvHCN);
-    printf("Dien tich HCN: %.2f\n", dtHCN);
-
-    // 2. Tinh hinh tron
-    printf("Nhap ban kinh: ");
-    scanf("%f", &r);
-
-    cvTron = 2 * PI * r;
-    dtTron = PI * r * r;
-
-    printf("Chu vi hinh tron: %.2f\n", cvTron);
-    printf("Dien tich hinh tron: %.2f\n", dtTron);
-
+    float dai,rong,r;
+    float cvHCN,dtHCN,cvHT,dtHT;
+    printf("Nhapchieudai:");
+    scanf("%f",&dai);
+    printf("Nhapchieurong:");
+    scanf("%f",&rong);
+    cvHCN = (dai+rong)* 2;
+    dtHCN = dai*rong;
+    printf("ChuviHCN:%f\n",cvHCN);
+    printf("DientichHCN:%f\n",dtHCN);
+    printf("Nhapbankinh:");
+    scanf("%f",&r);
+    cvHT = 2*PI*r;
+    dtHT = PI*r*r;
+    printf("ChuviHT:%f\n",cvHT);
+    printf("DientichHT:%f\n",dtHT);
     return 0;
+
+
+   
 }
